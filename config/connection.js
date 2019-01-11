e33e2f139b913317c71969fc08cd1cdb105c808b
Dan Staggs <staggs.dan@gmail.com>
2019-01-11 15:00:41 -0600

    connection string in connection.js file

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,6 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: "8889",
-  user: "root",
-  password: "root",
-  database: "sequelize_burgers_db"
-});
+var connection = mysql.createConnection('mysql://nj90y6j5tkv5yanh:rwwfqb2pqb2yopb7@xefi550t7t6tjn36.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/aw7a5egw9tf0lcap');
 
 connection.connect(function(err) {
   if (err) {
